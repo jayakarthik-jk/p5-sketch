58ce28a8556d9e270dd3f4ef7be849fa5d53399c
jayakarthik-jk <karthick72002@gmail.com>
2023-03-18 15:33:09 +0530

    code updated

--- balls/sketch.js
+++ balls/sketch.js
@@ -1,51 +1,48 @@
-let slider;
+const balls = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-const balls = [];
+function mousePressed() {
+  balls.push(new Ball(mouseX, mouseY));
+  if (balls.length > 100) {
+    balls.shift();
+  }
+}
 
 function draw() {
   background(0);
   for (const ball of balls) {
-    ball.render();
+    ball.move();
+    ball.edge();
+    ball.display();
   }
-  if (balls.length > 30) {
-    balls.splice(0, 1);
-  }
-}
-
-function mousePressed() {
-  const ball = new Ball(mouseX, mouseY);
-  balls.push(ball);
 }
 
 class Ball {
-  constructor(x, y, r = random(30, 50), brightness = 255) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.brightness = brightness;
-    this.direction = 1;
-    this.speed = 0;
-    this.gravity = 0.5;
+  constructor(x = random(width), y = random(height), radius = random(20, 50)) {
+    this.location = createVector(x, y);
+    this.velocity = createVector(0, 1);
+    this.accelaration = createVector(0, 1);
+    this.radius = radius;
   }
-  render() {
+  move() {
+    this.velocity.add(this.accelaration);
+    this.location.add(this.velocity);
+  }
+  edge() {
+    if (this.location.x >= width) {
+      this.location.x = width;
+      this.velocity.x *= -1;
+    }
+    if (this.location.y >= height) {
+      this.location.y = height;
+      this.velocity.y *= -1;
+    }
+  }
+  display() {
     noStroke();
-    fill(this.brightness);
-    ellipse(this.x, this.y, this.r);
-    this.y += this.speed;
-    this.x += this.direction * 5;
-    if (this.x + this.r / 2 > width) {
-      this.direction = -1;
-    }
-    if (this.x + this.r / 2 < 0) {
-      this.direction = 1;
-    }
-    this.speed += this.gravity;
-    if (this.y > height) {
-      this.y = height;
-      this.speed *= -0.9;
-    }
+    fill(255);
+    ellipse(this.location.x, this.location.y, this.radius);
   }
 }
